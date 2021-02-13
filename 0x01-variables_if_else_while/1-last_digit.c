4f85c32a096e427fd51de32b88d7571632e56f48
Melisa Rojas <melisarv@gmail.com>
2021-02-13 12:31:43 -0500

    Udpate script - correction

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,12 +17,12 @@ int main(void)
 	last = (n % 10);
 	printf("Last digit of %i is %i", n, last);
 
-	if (last == 0)
-		printf(" and is 0\n");
+	if (last > 5)
+		printf(" and is greater than 5\n");
 	else if (last < 6 && last != 0)
 		printf(" and is less than 6 and not 0\n");
 	else
-		printf(" and is greater than 5\n");
+		printf(" and is 0\n");
 
 	return (0);
 }
