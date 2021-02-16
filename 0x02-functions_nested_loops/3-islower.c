d5a7d6a551434917e7d5874257c7b6b1cc2acb57
Melisa Rojas <melisarv@gmail.com>
2021-02-15 22:28:08 -0500

    create function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - function that checks for lowercase
+ *
+ * Return:0
+ *
+ */
+
+int _islower(int c)
+{
+	int r;
+	r = islower(c);
+
+	return (r);
+}
