061488b1de5ed3ea9787e654d3b315f0bfc00223
Melisa Rojas <melisarv@gmail.com>
2021-02-16 12:35:32 -0500

    function update - add description

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,12 @@
 #include "holberton.h"
 
 /**
- * reset_to_98 - entry point 
+ * reset_to_98 - entry point
  *
  * Description: function that takes a pointer and update the value to 98
- * 
+ *
+ * @n: point to be updated
+ *
  * Return: void
  *
  */
