df095696a61e0d41cc7dedff78e1b67d9ff737c6
Melisa Rojas <melisarv@gmail.com>
2021-02-15 21:43:41 -0500

    create function that prints the alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * main - prints 10 times the alphabet in lowercase
+ *
+ * Return:0
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+# 0x02-functions_nested_loops
+In this project web learn nested loops, functions, prototype and header
