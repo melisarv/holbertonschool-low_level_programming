270df8560c3ae058ce1502dbafe3ce8e6ac1d450
Melisa Rojas <melisarv@gmail.com>
2021-02-16 11:44:35 -0500

    create a function that takes a pointer and updates the value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+
+/**
+ * main - function that takes a pointer and update the value to 98
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+# 0x05-pointers_arrays_string
+In this project we learn about pointers, arrays and strings
