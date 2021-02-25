318c3a6129f03226e353216e4fb021f8598af8e1
Melisa Rojas <melisarv@gmail.com>
2021-02-24 19:59:34 -0500

    create function prints a string in reverse, returns the length, factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * factorial - function that returns the factorial
+ * @n: integer number
+ * Return: integer number factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+
+	return (n * factorial(n - 1));
+}
