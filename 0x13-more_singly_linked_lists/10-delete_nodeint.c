a0a9d9695dd2ce217842ee0d0c5791df40a9059d
Melisa Rojas <melisarv@gmail.com>
2021-03-23 22:58:59 -0500

    create function that deletes the node at index of a list

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+
+/**
+ * pop_listint - function that delete the head node of a list
+ * @head: the list
+ * Return: integer
+ */
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *temp;
+
+	if (*head == NULL)
+	return (0);
+
+	temp = (*head)->next;
+	n = (*head)->n;
+	free(*head);
+	*head = temp;
+	return (n);
+}
+
+/**
+ * delete_nodeint_at_index - deletes the node at index of a linked list
+ * @head: pointer to the listint_t
+ * @index: index of the node
+ * Return: integer
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *firstnode = *head;
+	listint_t *temp = NULL;
+
+	if (index == 0)
+		return pop_listint(head);
+	
+	while (i < index - 1)
+	{
+		if (firstnode->next == NULL)
+		{
+			return (-1);
+		}
+		i++;
+		firstnode = firstnode->next;
+	}
+
+	temp = firstnode->next;
+	firstnode->next = temp->next;
+	free(temp);
+	return (1);
+}
