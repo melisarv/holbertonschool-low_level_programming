b5b27ee1ee500058a26a4293887fb51490015566
Melisa Rojas <melisarv@gmail.com>
2021-03-23 23:43:47 -0500

    function update

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,17 +11,20 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		unsigned int i = 0;
 
 		listint_t *firstnode = head;
-		listint_t *getnode = NULL;
 
-		while (i < index - 1)
+		if (head == NULL)
 		{
-			if (firstnode->next == NULL)
+			return (NULL);
+		}
+
+		while (firstnode)
+		{
+			if (i == index)
 			{
-				return (NULL);
+				return (firstnode);
 			}
 			i++;
 			firstnode = firstnode->next;
 		}
-		getnode = firstnode->next;
-		return (getnode);
+		return (NULL);
 }
