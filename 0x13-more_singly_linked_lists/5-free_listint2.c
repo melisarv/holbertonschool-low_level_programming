cb69862de3c71f80323a698ca1b50842cde73678
Melisa Rojas <melisarv@gmail.com>
2021-03-23 22:54:19 -0500

    create function that frees a list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that free a list
+ * @head: the list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	while (*head != NULL)
+	{
+		listint_t *temp = (*head)->next;
+
+		free(*head);
+		*head = temp;
+	}
+}
