8b408ad975454475406f77714c732963d6b5cade
Melisa Rojas <melisarv@gmail.com>
2021-03-27 14:15:03 -0500

    function update

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,36 +8,9 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int new;
-
-	if (index > 64)
+	if (index > (sizeof(*n) * 8))
 		return (-1);
 
-	new = *n;
-	new = get_bit(new, index);
-
-	if (new == 0x0)
-		return (1);
-	*n = (*n) - power(2, index);
+	*n = (*n) & ~(1 << index);
 	return (1);
 }
-
-/**
- * power - power of base
- * @base: base
- * @expo: exponent
- * Return: integer
- */
-unsigned int power(unsigned int base, unsigned int expo)
-{
-	unsigned int i = 0;
-	unsigned int raised = 1;
-
-	if (expo == 0)
-		return (1);
-
-	for (i = 0; i > expo; i++)
-		raised = raised * base;
-	return (raised);
-}
-
