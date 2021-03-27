d9d2abaf6d8b6daf657558cc3fcc00cd0c5b51bc
Melisa Rojas <melisarv@gmail.com>
2021-03-27 14:10:23 -0500

    function update

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,16 +8,9 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int new;
-
 	if (index >= (sizeof(*n) * 8))
 		return (-1);
 
-	new = *n;
-	new = get_bit(new, index);
-
-	if (new == 0x1)
-		return (1);
 	*n = (*n) | 1 << index;
 	return (1);
 }
