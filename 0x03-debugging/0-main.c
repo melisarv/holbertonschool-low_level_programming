a5519fab3064de472579d84bf8b1cc6978077d4a
Melisa Rojas <melisarv@gmail.com>
2021-04-16 20:46:06 -0500

    create function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ *main - Entry point
+ *
+ *Description: trying function that prints if integer is positive or negative.
+ *positive, negative, or zero.
+ *Return: Returns 0.
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+# 0x03-debugging
+In this project web learn debugging
