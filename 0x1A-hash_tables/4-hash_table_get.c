89feeaf5006c4b57d698e32de799258e2adfd7b9
Melisa Rojas <melisarv@gmail.com>
2021-06-01 01:02:31 -0500

    fix condition if

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,7 +10,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *hn = NULL;
 	unsigned long int index;
 
-	if (ht == NULL && key == NULL)
+	if (ht == NULL || key == NULL)
 		return (NULL);
 
 	index = key_index((unsigned char *)key, ht->size);
