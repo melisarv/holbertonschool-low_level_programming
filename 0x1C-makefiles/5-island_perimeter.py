fe0334b4889c765a11259b7734443a1988bcaef3
Melisa Rojas <melisarv@gmail.com>
2021-07-02 20:27:44 -0500

    add task 3, 4 and 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""function island_perimeter"""
+
+
+def island_perimeter(grid):
+    """ Returns the perimeter of the island described in grid """
+    rows = len(grid)
+    cols = len(grid[0])
+    perimeter = 0
+
+    if not grid:
+        return (0)
+
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == 1:
+                if (c - 1) < 0 or grid[r][c-1] == 0:
+                    perimeter += 1
+                if (c + 1) == cols or grid[r][c+1] == 0:
+                    perimeter += 1
+                if (r - 1) < 0 or grid[r-1][c] == 0:
+                    perimeter += 1
+                if (r + 1) == rows or grid[r+1][c] == 0:
+                    perimeter += 1
+
+    return perimeter
